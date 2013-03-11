71a67f57ce3a4092ca2aaf66d16a0ef7be28f7f0
Evan Larkin <evan.larkin@vibes.com>
2013-03-11 09:43:01 -0500

    Adding Ruby modes, spaces instead of tabs, etc.

--- init.el
+++ init.el
@@ -16,12 +16,13 @@
 
 ;; install these packages if they are not there.
 (defvar my-packages '(clojure-mode
-		      clojure-test-mode
-		      nrepl
-		      paredit
-		      rainbow-delimiters
-		      color-theme
-		      color-theme-solarized))
+          clojure-test-mode
+          nrepl
+          paredit
+          rainbow-delimiters
+          rinari
+          color-theme
+          color-theme-solarized))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
@@ -35,6 +36,8 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
+(color-theme-solarized-dark)
+(set-fringe-mode -1)
 
 ;; clipboard
 (setq x-select-enable-clipboard t)
@@ -46,9 +49,9 @@
 (setq display-time-24hr-format t)
 (setq display-time-day-and-date t)
 (display-time)
-(color-theme-solarized-dark)
 (global-font-lock-mode t)
 (setq-default show-trailing-whitespace t)
+(server-mode 1)
 
 ;; Caret Settings
 (blink-cursor-mode -1)
@@ -60,16 +63,24 @@
 
 ;; Indentation Settings
 (setq-default indent-tabs-mode nil) ; use spaces
+(setq-default tab-width 2)
 
 ;; reload buffers changed on disk without unsaved changes
 (setq auto-revert-verbose nil)
 (setq auto-revert-interval 3)
 (global-auto-revert-mode 1)
 
+;; Backups should not clutter my file system
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
 ;; Mark settings
 (transient-mark-mode -1)
 
 ;; Hooks
+;; Lisp Modes
 (defun lisp-minor-modes ()
   (progn
     (paredit-mode t)
@@ -81,10 +92,24 @@
 (add-hook 'clojure-mode-hook 'lisp-minor-modes)
 (add-hook 'nrepl-mode-hook 'lisp-minor-modes)
 
+;; Ruby Modes
+(defun ruby-minor-modes ()
+  (progn
+    (rinari-minor-mode t)
+    (rainbow-delimiters-mode t)))
+
+(add-hook 'ruby-mode-hook 'ruby-minor-modes)
+
 ;; Keybindings
 ;; Paredit
 (require 'paredit)
 (define-key paredit-mode-map "\M-[" 'paredit-wrap-square)
 (define-key paredit-mode-map "\M-{" 'paredit-wrap-curly)
 
-
+;; EmacsClient should be quit with C-c C-c instead of C-x #
+(add-hook 'server-switch-hook
+          (lambda ()
+            (when (current-local-map)
+              (use-local-map (copy-keymap (current-local-map))))
+            (when server-buffer-clients
+              (local-set-key (kbd "C-c C-c") 'server-edit))))
