fc7857f8ae5f219c145ef3743fdc464d67b87055
Evan Larkin <evan.larkin@caest.us>
2013-03-20 22:30:17 -0500

    Accidentally committed some custom stuff. Removing it.

--- init.el
+++ init.el
@@ -143,15 +143,3 @@
 (add-hook 'evil-mode-hook
           (lambda ()
             (undo-tree-mode t)))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("/Users/evan.larkin/org/notes.org"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
