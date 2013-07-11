d654365b7092f4d4bc5748e5338b5a99fcf07613
Evan Larkin <evan.larkin@vibes.com>
2013-07-11 16:31:08 -0500

    Adding eshell/clear

--- initializers/eshell.el
+++ initializers/eshell.el
@@ -4,3 +4,9 @@
     (setq show-trailing-whitespace nil)))
 
 (add-hook 'eshell-mode-hook 'eshell-settings)
+
+;; Eshell Commands
+(defun eshell/clear ()
+  (interactive)
+  (let ((inhibit-read-only t))
+    (erase-buffer)))
