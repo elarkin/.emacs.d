5f024493bc6e8240275f3aa04b0a0c1f589bfb72
Evan Larkin <evan.larkin@vibes.com>
2013-07-11 16:24:44 -0500

    Ruby mode should activate on ruby files that have custom extensions

--- initializers/ruby-mode.el
+++ initializers/ruby-mode.el
@@ -2,6 +2,20 @@
 (defun ruby-minor-modes ()
   (progn
     (rinari-minor-mode t)
-    (rainbow-delimiters-mode t)))
+    (rainbow-delimiters-mode t)
+    (make-local-variable 'reindent-or-completion-completion)
+    (setq reindent-or-completion-completion 'dabbrev-expand)
+    (local-set-key (kbd "<tab>") 'reindent-or-completion)
+    (local-set-key (kbd "<C-tab>") 'indent-for-tab-command)))
 
 (add-hook 'ruby-mode-hook 'ruby-minor-modes)
+
+;; ruby mode should apply to all ruby files
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rabl$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.irbrc$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
