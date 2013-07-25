518629a8ffc96264b0dc3af50a2f254be40e1ac7
Evan Larkin <evan.larkin@vibes.com>
2013-07-25 09:32:30 -0500

    Using eshell smart display

--- initializers/eshell.el
+++ initializers/eshell.el
@@ -1,7 +1,15 @@
+;; Smart Mode
+(require 'eshell)
+(require 'em-smart)
+(setq eshell-where-to-jump 'begin)
+(setq eshell-review-quick-commands t)
+(setq eshell-smart-space-goes-to-end t)
+
 ;; Eshell Hooks
 (defun eshell-settings ()
   (progn
-    (setq show-trailing-whitespace nil)))
+    (setq show-trailing-whitespace nil)
+    (eshell-smart-initialize)))
 
 (add-hook 'eshell-mode-hook 'eshell-settings)
 
